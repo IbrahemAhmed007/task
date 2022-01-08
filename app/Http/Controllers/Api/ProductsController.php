f96e1a17a59f6db43f3c1330b9cfc1aa53c564ae
ibrahim <ibrahem.ahmed2012@gmail.com>
2022-01-08 17:46:19 +0200

    filter Products API

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -10,9 +10,24 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function list(): JsonResponse
+    public function list(Request $request): JsonResponse
     {
-        $products = Product::select(['id', 'name', 'image', 'price', 'old_price', 'description', 'qty'])->get();
+        $categoryId = $request->get('category_id');
+        $priceFrom = $request->get('price_from');
+        $priceTo = $request->get('price_to');
+
+        $products = Product::
+        where(function ($query) use ($categoryId) {
+            if ($categoryId)
+                $query->where('category_id', $categoryId);
+        })->where(function ($query) use ($priceFrom) {
+            if ($priceFrom)
+                $query->where('price', '>=', $priceFrom);
+        })->where(function ($query) use ($priceTo) {
+            if ($priceTo)
+                $query->where('price', '<=', $priceTo);
+        })->
+        select(['id', 'name', 'image', 'price', 'old_price', 'description', 'qty', 'category_id'])->get();
         return response()->json(['products' => ProductResource::collection($products)]);
     }
 
